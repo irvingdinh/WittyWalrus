2a2ffe90e7db6ebb198aca63c48995ea6564c732
Irving Dinh <irving.dinh@gmail.com>
2023-08-31 11:32:34 +0700

    Initial commit (cont.)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,12 +7,12 @@ import '@/styles/tailwind.css'
 
 export const metadata: Metadata = {
   title: {
-    template: '%s - Spencer Sharp',
+    template: '%s - Irving Dinh',
     default:
-      'Spencer Sharp - Software designer, founder, and amateur astronaut',
+      'Irving Dinh - Software engineer, designer, and amateur photographer',
   },
   description:
-    'I’m Spencer, a software designer and entrepreneur based in New York City. I’m the founder and CEO of Planetaria, where we develop technologies that empower regular people to explore space on their own terms.',
+    'I’m Irving, a software engineer based in Ho Chi Minh City. I’m at the helm of a game development studio named after myself, where we craft casual games for fun-seekers!',
   alternates: {
     types: {
       'application/rss+xml': `${process.env.NEXT_PUBLIC_SITE_URL}/feed.xml`,
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -273,32 +273,33 @@ export default async function Home() {
       <Container className="mt-9">
         <div className="max-w-2xl">
           <h1 className="text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
-            Software designer, founder, and amateur astronaut.
+            Software engineer, designer, and amateur photographer.
           </h1>
           <p className="mt-6 text-base text-zinc-600 dark:text-zinc-400">
-            I’m Spencer, a software designer and entrepreneur based in New York
-            City. I’m the founder and CEO of Planetaria, where we develop
-            technologies that empower regular people to explore space on their
-            own terms.
+            I’m Irving, a software engineer based in Ho Chi Minh City.
+            I wear multiple hats as the founder and CEO of a game development
+            studio that proudly carries my name. At our studio, we specialize
+            in crafting casual games designed to bring joy and entertainment
+            to fun-seekers everywhere!
           </p>
           <div className="mt-6 flex gap-6">
+            {/*<SocialLink*/}
+            {/*  href="https://twitter.com"*/}
+            {/*  aria-label="Follow on Twitter"*/}
+            {/*  icon={TwitterIcon}*/}
+            {/*/>*/}
             <SocialLink
-              href="https://twitter.com"
-              aria-label="Follow on Twitter"
-              icon={TwitterIcon}
-            />
-            <SocialLink
-              href="https://instagram.com"
+              href="https://instagram.com/irvingdinh"
               aria-label="Follow on Instagram"
               icon={InstagramIcon}
             />
             <SocialLink
-              href="https://github.com"
+              href="https://github.com/irvingdinh"
               aria-label="Follow on GitHub"
               icon={GitHubIcon}
             />
             <SocialLink
-              href="https://linkedin.com"
+              href="https://linkedin.com/in/irvingdinh"
               aria-label="Follow on LinkedIn"
               icon={LinkedInIcon}
             />
@@ -306,19 +307,19 @@ export default async function Home() {
         </div>
       </Container>
       <Photos />
-      <Container className="mt-24 md:mt-28">
-        <div className="mx-auto grid max-w-xl grid-cols-1 gap-y-20 lg:max-w-none lg:grid-cols-2">
-          <div className="flex flex-col gap-16">
-            {articles.map((article) => (
-              <Article key={article.slug} article={article} />
-            ))}
-          </div>
-          <div className="space-y-10 lg:pl-16 xl:pl-24">
-            <Newsletter />
-            <Resume />
-          </div>
-        </div>
-      </Container>
+      {/*<Container className="mt-24 md:mt-28">*/}
+      {/*  <div className="mx-auto grid max-w-xl grid-cols-1 gap-y-20 lg:max-w-none lg:grid-cols-2">*/}
+      {/*    <div className="flex flex-col gap-16">*/}
+      {/*      {articles.map((article) => (*/}
+      {/*        <Article key={article.slug} article={article} />*/}
+      {/*      ))}*/}
+      {/*    </div>*/}
+      {/*    <div className="space-y-10 lg:pl-16 xl:pl-24">*/}
+      {/*      <Newsletter />*/}
+      {/*      <Resume />*/}
+      {/*    </div>*/}
+      {/*  </div>*/}
+      {/*</Container>*/}
     </>
   )
 }
